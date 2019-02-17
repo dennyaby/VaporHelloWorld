5465eed3cdb5b5059d01d6b23a3ba621413767f6
dennya <dennya.by@gmail.com>
2019-02-17 14:32:37 +0300

    User MySQL as database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
+import FluentMySQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentMySQLProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
@@ -18,15 +18,25 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    
 
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "hostname"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    
+    let databaseConfig = MySQLDatabaseConfig(
+        hostname: hostname,
+        username: username,
+        password: password,
+        database: databaseName)
+    let database = MySQLDatabase(config: databaseConfig)
+    databases.add(database: database, as: .mysql)
     services.register(databases)
-
-    // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    // 4
+    migrations.add(model: Acronym.self, database: .mysql)
     services.register(migrations)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -32,10 +32,4 @@ public func routes(_ router: Router) throws {
     router.post(InfoData.self, at: "info") { request, data -> InfoResponse in
         return InfoResponse(request: data)
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
